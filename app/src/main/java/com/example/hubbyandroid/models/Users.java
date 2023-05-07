9087b7063747604d00cc63528b6158510cbd9597
FernandoSodre <111505678+FernandoSodre@users.noreply.github.com>
2023-05-07 13:59:31 -0300

    First Commit

---
+++ app/src/main/java/com/example/hubbyandroid/models/Users.java
@@ -0,0 +1,36 @@
+package com.example.hubbyandroid.models;
+
+public class Users {
+    int id;
+    String nickname;
+    String password;
+    String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ app/src/main/java/com/example/hubbyandroid/views/MainMenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.hubbyandroid.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hubbyandroid.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
