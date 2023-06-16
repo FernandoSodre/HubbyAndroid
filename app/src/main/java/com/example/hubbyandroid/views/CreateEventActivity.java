62d41d4eaeb7230dceec3cf473316302d076e969
Brendo Douglas <brendodouglasmo@hotmail.com>
2023-06-16 20:35:44 -0300

    Ajustado tela de criar evento

--- app/src/main/java/com/example/hubbyandroid/views/CreateEventActivity.java
+++ app/src/main/java/com/example/hubbyandroid/views/CreateEventActivity.java
@@ -3,6 +3,11 @@ package com.example.hubbyandroid.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
 
 import com.example.hubbyandroid.R;
 import com.google.firebase.database.DataSnapshot;
@@ -11,13 +16,21 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.MutableData;
 import com.google.firebase.database.Transaction;
+import com.google.firebase.database.core.view.View;
+
+import java.util.ArrayList;
 
 public class CreateEventActivity extends AppCompatActivity {
+
+    private Spinner spinnerSelectCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+        Log.d("estate", "indo criar");
+
+        spinnerSelectCategory = findViewById(R.id.spinnerSelectCategory);
     }
 
     private void criarEvento(){
@@ -40,6 +53,22 @@ public class CreateEventActivity extends AppCompatActivity {
                 if (committed){
                     Integer incrementedValue = currentData.getValue(Integer.class);
                 }
+            }
+        });
+    }
+
+    private void populateSpinner()
+    {
+        spinnerSelectCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, android.view.View view, int position, long id) {
+                // Lógica para tratar a opção selecionada
+                String selectedOption = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Lógica quando nenhuma opção é selecionada (opcional)
             }
         });
     }
