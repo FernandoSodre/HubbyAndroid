688aead96faed9be290daa4b36a7d20ea822066c
HeckBranchi <75676176+HeckBranchi@users.noreply.github.com>
2023-06-16 22:51:02 -0300

    Implementação do CheckBox

--- app/src/main/java/com/example/hubbyandroid/views/LoginActivity.java
+++ app/src/main/java/com/example/hubbyandroid/views/LoginActivity.java
@@ -3,9 +3,11 @@ package com.example.hubbyandroid.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +26,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText editTextSenha;
     private Button loginButton;
     private TextView linkTextView;
+    private CheckBox checkBoxManterLogado;
+    public static final String SHARED_PREFS = "shared_pref";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,9 @@ public class LoginActivity extends AppCompatActivity {
         editTextSenha = findViewById(R.id.editSenha);
         loginButton = findViewById(R.id.buttonLogin);
         linkTextView = findViewById(R.id.login);
+        checkBoxManterLogado = findViewById(R.id.boxManterLogado);
+
+        checkBox();
 
         linkTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +61,24 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void checkBox() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String checkBox = sharedPreferences.getString("pref_check", "");
+        if(checkBox.equals("true")) {
+            startActivity(new Intent(LoginActivity.this, MainMenuActivity.class));
+            Toast.makeText(LoginActivity.this, "Login efetuado com sucesso!.",
+                        Toast.LENGTH_SHORT).show();
+            finish();
+
+        } else {
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.clear().apply();
+
+        }
+
+    }
+
     private FirebaseAuth mAuth;
 
     public FirebaseAuth getmAuth() {
@@ -68,10 +93,15 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+
                             FirebaseUser user = mAuth.getCurrentUser();
-
+                            if(checkBoxManterLogado.isChecked()){
+                                SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("pref_check", "true");
+                                editor.apply();
+                            }
                             startActivity(new Intent(LoginActivity.this, MainMenuActivity.class));
-
                             Toast.makeText(LoginActivity.this, "Login efetuado com sucesso!.",
                                     Toast.LENGTH_SHORT).show();
                         } else{
